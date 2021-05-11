9e9f123c9de91287e5d33aea6daaf6a984604a6c
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2021-05-11 17:16:13 +0000

    Added space

--- data_aug.py
+++ data_aug.py
@@ -12,3 +12,7 @@ def load_data(path):
     images = sorted(glob(os.path.join(path, "images/*")))
     masks = sorted(glob(os.path.join(path, "masks/*")))
     return images, masks
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
